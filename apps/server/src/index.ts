89c02a214d6b81b1605e48745810e87c70c2312b
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2025-02-13 19:28:22 +0530

    integrate next-auth

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -7,9 +7,11 @@ app.use(express.json());
 const httpServer = app.listen(8080);
 
 const wss = new WebSocketServer({ server: httpServer });
+console.log("Starting websocket server on port 8080");
+console.log("Access at: ws://127.0.0.1:8080/")
 
 wss.on("connection", function connection(ws) {
-    ws.on('error', console.error);.
+    ws.on('error', console.error);
 
     ws.on('message', function message(data: string, isBinary: boolean | undefined) {
         wss.clients.forEach(function each(client) {
